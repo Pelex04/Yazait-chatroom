3e5a72fe8e0c6e914d9664ff0bd4e58ee88bb1db
pelex04 <kingsleychideru1404@gmail.com>
2026-02-03 15:16:28 +0200

    removed blank on target2

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { io, Socket } from "socket.io-client";
 
-const SOCKET_URL = "http://localhost:5000/";
+const SOCKET_URL = "https://chatroom-0u60.onrender.com/";
 
 class SocketService {
   private socket: Socket | null = null;
